70736891eb262ee6c81274a6fdbfd65062aa7ad7
Esteban Garcia <gmgarciam@gmail.com>
2018-11-10 16:08:18 -0800

    Ldkbagel testing (#14)
    * Last push before files restructured.
    
    * last push before folder restructure
    
    * Last push folders
    
    * Folder hiearchy change, i'll do the rest tomorrow
    
    * applying patch bagelfix to ldkbageltesting
    
    * ready for merging

---
+++ Source/myICHU/myICHUGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "myICHUGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYICHU_API AmyICHUGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
