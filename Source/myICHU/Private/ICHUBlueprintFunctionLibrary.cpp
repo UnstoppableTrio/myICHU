7606f79060783d12f8765510ca7bab679a59b463
Esteban Garcia <gmgarciam@gmail.com>
2018-11-26 11:00:35 -0800

    last upload before switch to alex patch

--- Source/myICHU/Private/ICHUBlueprintFunctionLibrary.cpp
+++ Source/myICHU/Private/ICHUBlueprintFunctionLibrary.cpp
@@ -2,7 +2,32 @@
 
 
 #include "ICHUBlueprintFunctionLibrary.h"
+#include "myICHU.h"
+#include "ICHUBlueprintFunctionLibrary.h"
+#include "ActionRPGLoadingScreen.h"
 
+
+URPGBlueprintLibrary::URPGBlueprintLibrary(const FObjectInitializer& ObjectInitializer)
+        : Super(ObjectInitializer)
+{
+}
+
+void URPGBlueprintLibrary::PlayLoadingScreen(bool bPlayUntilStopped, float PlayTime)
+{
+    IActionRPGLoadingScreenModule& LoadingScreenModule = IActionRPGLoadingScreenModule::Get();
+    LoadingScreenModule.StartInGameLoadingScreen(bPlayUntilStopped, PlayTime);
+}
+
+void URPGBlueprintLibrary::StopLoadingScreen()
+{
+    IActionRPGLoadingScreenModule& LoadingScreenModule = IActionRPGLoadingScreenModule::Get();
+    LoadingScreenModule.StopInGameLoadingScreen();
+}
+
+bool URPGBlueprintLibrary::IsInEditor()
+{
+    return GIsEditor;
+}
 
 
 
